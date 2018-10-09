5b5aaa1ad5e3d12c4f8edd572ee3ece872c11b54
Evgeniy Kochetov <evgeniik@mellanox.com>
2018-10-09 21:13:12 +0000

    NVMf offload: the code is now buildable and runnable with stubs for offload API.

--- test/unit/lib/nvmf/ctrlr.c/ctrlr_ut.c
+++ test/unit/lib/nvmf/ctrlr.c/ctrlr_ut.c
@@ -138,6 +138,11 @@ DEFINE_STUB(spdk_nvmf_subsystem_listener_allowed,
 	    (struct spdk_nvmf_subsystem *subsystem, struct spdk_nvme_transport_id *trid),
 	    true);
 
+DEFINE_STUB(spdk_nvmf_subsystem_get_offload,
+	    bool,
+	    (const struct spdk_nvmf_subsystem *subsystem),
+	    false);
+
 static void
 ctrlr_ut_pass_msg(spdk_thread_fn fn, void *ctx, void *thread_ctx)
 {
