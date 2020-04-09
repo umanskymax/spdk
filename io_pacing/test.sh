1375be6f990d23c9c75e229db9f7e3cd2a8d227c
Evgeniy Kochetov <evgeniik@mellanox.com>
2020-04-09 08:48:10 +0300

    More test automation
    Target is now started and configured with RPC automatically. No need
    for config files.
    
    Signed-off-by: Evgeniy Kochetov <evgeniik@mellanox.com>

--- io_pacing/test.sh
+++ io_pacing/test.sh
@@ -15,16 +15,16 @@ SPDK_PATH="$PWD/.."
 FIO_JOBS_PATH="$PWD/jobs"
 OUT_PATH="$PWD/out"
 mkdir -p $OUT_PATH
+TARGET_SPDK_PATH="/home/evgeniik/spdk"
 
 # Other configurations
 ENABLE_DEVICE_COUNTERS=1
 
 # Internal variables
-FIO_PARAMS="--time_based=1 --runtime=$TEST_TIME --readwrite=$RW --bs=$IO_SIZE --iodepth=$QD --output-format=json"
 
 function m()
 {
-    bc <<< "scale=1; $@"
+    bc 2>/dev/null <<< "scale=1; $@"
 }
 
 function get_device_counters()
@@ -36,13 +36,15 @@ function parse_fio()
 {
     local LOG=$1; shift
 
-    IOPS_R=$(jq .jobs[].read.iops $LOG)
-    BW_R=$(jq .jobs[].read.bw_bytes $LOG)
-    LAT_AVG_R=$(jq .jobs[].read.lat_ns.mean $LOG)
+    IOPS_R=$(jq .jobs[].read.iops $LOG 2>/dev/null)
+    BW_R=$(jq .jobs[].read.bw_bytes $LOG 2>/dev/null)
+    BW_STDDEV_R=$(jq .jobs[].read.bw_dev $LOG 2>/dev/null)
+    LAT_AVG_R=$(jq .jobs[].read.lat_ns.mean $LOG 2>/dev/null)
 
-    IOPS_W=$(jq .jobs[].write.bw_bytes $LOG)
-    BW_W=$(jq .jobs[].write.iops $LOG)
-    LAT_AVG_W=$(jq .jobs[].write.lat_ns.mean $LOG)
+    IOPS_W=$(jq .jobs[].write.bw_bytes $LOG 2>/dev/null)
+    BW_W=$(jq .jobs[].write.iops $LOG 2>/dev/null)
+    BW_STDDEV_W=$(jq .jobs[].write.bw_dev $LOG 2>/dev/null)
+    LAT_AVG_W=$(jq .jobs[].write.lat_ns.mean $LOG 2>/dev/null)
 }
 
 function print_report()
@@ -50,6 +52,7 @@ function print_report()
     local HOSTS=$@
     local SUM_IOPS_R=0
     local SUM_BW_R=0
+    local SUM_BW_STDDEV_R=0
     local SUM_LAT_AVG_R=0
 
     echo "Test parameters"
@@ -61,8 +64,8 @@ function print_report()
     echo ""
 
     echo Results
-    local FORMAT="%-30s | %-10s | %-10s | %-15s | %-15s\n"
-    printf "$FORMAT" "Host" "kIOPS" "BW,Gb/s" "AVG_LAT,us" "Wire BW,Gb/s"
+    local FORMAT="%-30s | %-10s | %-10s | %-15s | %-15s | %-15s\n"
+    printf "$FORMAT" "Host" "kIOPS" "BW,Gb/s" "AVG_LAT,us" "Wire BW,Gb/s" "BW STDDEV"
     printf "$FORMAT" | tr " " "-"
 
     local count=0
@@ -70,19 +73,21 @@ function print_report()
 	parse_fio $OUT_PATH/fio-$host.json
 	SUM_IOPS_R=$(m $SUM_IOPS_R + $IOPS_R)
 	SUM_BW_R=$(m $SUM_BW_R + $BW_R)
+	SUM_BW_STDDEV_R=$(m $SUM_BW_STDDEV_R + $BW_STDDEV_R^2)
 	SUM_LAT_AVG_R=$(m $SUM_LAT_AVG_R + $LAT_AVG_R)
 
 	printf "$FORMAT" $host $(m $IOPS_R/1000) $(m $BW_R*8/1000^3) $(m $LAT_AVG_R/1000)
 	((count+=1))
     done
     SUM_LAT_AVG_R=$(m $SUM_LAT_AVG_R / $count)
+    SUM_BW_STDDEV_R=$(m "sqrt($SUM_BW_STDDEV_R / $count)")
 
     printf "$FORMAT" | tr " " "-"
 
     if [ "1" == "$ENABLE_DEVICE_COUNTERS" ]; then
-	local TX_BW_WIRE=$(jq '.analysis[].analysisAttribute | select(.name=="TX BandWidth") | .value' $OUT_PATH/device-counters.json)
+	local TX_BW_WIRE=$(jq '.analysis[].analysisAttribute | select(.name=="TX BandWidth") | .value' $OUT_PATH/device-counters.json 2>/dev/null)
     fi
-    printf "$FORMAT" "Total" $(m $SUM_IOPS_R/1000) $(m $SUM_BW_R*8/1000^3) "$(m $SUM_LAT_AVG_R/1000)" "$TX_BW_WIRE"
+    printf "$FORMAT" "Total" $(m $SUM_IOPS_R/1000) $(m $SUM_BW_R*8/1000^3) "$(m $SUM_LAT_AVG_R/1000)" "$TX_BW_WIRE" "$(m $SUM_BW_STDDEV_R*8/1000^2)"
 }
 
 function run_fio()
@@ -95,7 +100,9 @@ function run_fio()
     local SSH=
     [ "$HOST" != "$HOSTNAME" ] && SSH="ssh $HOST"
 
-    $SSH sudo LD_PRELOAD=$SPDK_PATH/install-$HOST/lib/fio_plugin $FIO_PATH/install-$HOST/bin/fio --output=$OUT_PATH/fio-$HOST.json $FIO_PARAMS $JOB
+    local FIO_PARAMS="--time_based=1 --runtime=$TEST_TIME --ramp_time=3 --readwrite=$RW --bs=$IO_SIZE --iodepth=$QD --output-format=json --output=$OUT_PATH/fio-$HOST.json"
+
+    $SSH sudo LD_PRELOAD=$SPDK_PATH/install-$HOST/lib/fio_plugin $FIO_PATH/install-$HOST/bin/fio $FIO_PARAMS $JOB
 }
 
 function progress_bar()
@@ -128,5 +135,254 @@ function run_test()
     wait $PIDS
 }
 
-run_test $HOSTS
-print_report $HOSTS
+function start_tgt()
+{
+    local CPU_MASK=$1; shift
+
+    ssh $TARGET sudo $TARGET_SPDK_PATH/install/bin/spdk_tgt --wait-for-rpc -m $CPU_MASK > $OUT_PATH/tgt.log 2>&1 &
+    sleep 10
+}
+
+function stop_tgt()
+{
+    ssh $TARGET 'sudo kill -15 $(pidof spdk_tgt)' >> $OUT_PATH/rpc.log 2>&1
+    sleep 5
+}
+
+function rpc()
+{
+    ssh $TARGET sudo $TARGET_SPDK_PATH/scripts/rpc.py -v $@ >> $OUT_PATH/rpc.log 2>&1
+    echo -n .
+}
+
+function rpc_start()
+{
+    [ -e rpc_pipe ] && rm rpc_pipe
+    mkfifo rpc_pipe
+    cat > rpc_pipe &
+    RPC_PID=$!
+    ssh $TARGET sudo $TARGET_SPDK_PATH/scripts/rpc.py -v --server >> $OUT_PATH/rpc.log 2>&1 < rpc_pipe &
+    SSH_PID=$!
+}
+
+function rpc_send() {
+    echo "$@" > rpc_pipe
+}
+
+function rpc_stop()
+{
+    kill $RPC_PID >> $OUT_PATH/rpc.log 2>&1
+    wait $SSH_PID
+    rm rpc_pipe
+}
+
+function basic_test()
+{
+    QD_LIST=${QD_LIST-"8 16 32 64 128 256"}
+    local FORMAT="%-10s | %-10s | %-10s | %-15s | %-10s\n"
+    printf "$FORMAT" "QD" "BW" "WIRE BW" "AVG LAT, us" "BW STDDEV"
+
+    for qd in $QD_LIST; do
+	QD=$qd run_test $HOSTS > /dev/null
+	OUT=$(print_report $HOSTS | tee $OUT_PATH/basic_test.log)
+	BW="$(echo "$OUT" | grep Total | awk '{print $5}')"
+	LAT_AVG="$(echo "$OUT" | grep Total | awk '{print $7}')"
+	WIRE_BW="$(echo "$OUT" | grep Total | awk '{print $9}')"
+	BW_STDDEV="$(echo "$OUT" | grep Total | awk '{print $11}')"
+	printf "$FORMAT" "$qd" "$BW" "$WIRE_BW" "$LAT_AVG" "$BW_STDDEV"
+    done
+}
+
+function config_null_1()
+{
+    NUM_SHARED_BUFFERS=${NUM_SHARED_BUFFERS-4095}
+    BUF_CACHE_SIZE=${BUF_CACHE_SIZE-32}
+    rpc_start
+    rpc_send nvmf_set_config --conn-sched transport
+    rpc_send framework_start_init
+    sleep 3
+    rpc_send nvmf_create_transport --trtype RDMA \
+	     --max-queue-depth 128 \
+	     --max-qpairs-per-ctrlr 64 \
+	     --in-capsule-data-size 4096 \
+	     --max-io-size 131072 \
+	     --io-unit-size 131072 \
+	     --num-shared-buffers $NUM_SHARED_BUFFERS \
+	     --buf-cache-size $BUF_CACHE_SIZE \
+	     --max-srq-depth 4096
+    rpc_send nvmf_create_subsystem --allow-any-host \
+	     --max-namespaces 48 \
+	     nqn.2016-06.io.spdk:cnode1
+    rpc_send nvmf_subsystem_add_listener --trtype rdma \
+	     --traddr 1.1.103.1 \
+	     --adrfam ipv4 \
+	     --trsvcid 4420 \
+	     nqn.2016-06.io.spdk:cnode1
+    rpc_send nvmf_subsystem_add_listener --trtype rdma \
+	     --traddr 2.2.103.1 \
+	     --adrfam ipv4 \
+	     --trsvcid 4420 \
+	     nqn.2016-06.io.spdk:cnode1
+    rpc_send bdev_null_create Null0 8192 4096
+    rpc_send nvmf_subsystem_add_ns nqn.2016-06.io.spdk:cnode1 Null0
+    rpc_stop
+    sleep 1
+}
+
+function config_null_16()
+{
+    NUM_SHARED_BUFFERS=${NUM_SHARED_BUFFERS-4095}
+    BUF_CACHE_SIZE=${BUF_CACHE_SIZE-32}
+    rpc_start
+    rpc_send nvmf_set_config --conn-sched transport
+    rpc_send framework_start_init
+    sleep 3
+    rpc_send nvmf_create_transport --trtype RDMA \
+	     --max-queue-depth 128 \
+	     --max-qpairs-per-ctrlr 64 \
+	     --in-capsule-data-size 4096 \
+	     --max-io-size 131072 \
+	     --io-unit-size 131072 \
+	     --num-shared-buffers $NUM_SHARED_BUFFERS \
+	     --buf-cache-size $BUF_CACHE_SIZE \
+	     --max-srq-depth 4096
+    rpc_send nvmf_create_subsystem --allow-any-host \
+	     --max-namespaces 48 \
+	     nqn.2016-06.io.spdk:cnode1
+    rpc_send nvmf_subsystem_add_listener --trtype rdma \
+	     --traddr 1.1.103.1 \
+	     --adrfam ipv4 \
+	     --trsvcid 4420 \
+	     nqn.2016-06.io.spdk:cnode1
+    rpc_send nvmf_subsystem_add_listener --trtype rdma \
+	     --traddr 2.2.103.1 \
+	     --adrfam ipv4 \
+	     --trsvcid 4420 \
+	     nqn.2016-06.io.spdk:cnode1
+    for i in $(seq 16); do
+	rpc_send bdev_null_create Null$i 8192 4096
+	rpc_send nvmf_subsystem_add_ns nqn.2016-06.io.spdk:cnode1 Null$i
+    done
+    rpc_stop
+    sleep 1
+}
+
+function config_nvme()
+{
+    NUM_SHARED_BUFFERS=${NUM_SHARED_BUFFERS-4095}
+    BUF_CACHE_SIZE=${BUF_CACHE_SIZE-32}
+    local DISKS="05 06 07 08 09 0a 0b 0c 0f 10 11 12 13 14 15 16"
+    rpc_start
+    rpc_send nvmf_set_config --conn-sched transport
+    rpc_send framework_start_init
+    sleep 3
+    rpc_send nvmf_create_transport --trtype RDMA \
+	     --max-queue-depth 128 \
+	     --max-qpairs-per-ctrlr 64 \
+	     --in-capsule-data-size 4096 \
+	     --max-io-size 131072 \
+	     --io-unit-size 131072 \
+	     --num-shared-buffers $NUM_SHARED_BUFFERS \
+	     --buf-cache-size $BUF_CACHE_SIZE \
+	     --max-srq-depth 4096
+    rpc_send nvmf_create_subsystem --allow-any-host \
+	     --max-namespaces 48 \
+	     nqn.2016-06.io.spdk:cnode1
+    rpc_send nvmf_subsystem_add_listener --trtype rdma \
+	     --traddr 1.1.103.1 \
+	     --adrfam ipv4 \
+	     --trsvcid 4420 \
+	     nqn.2016-06.io.spdk:cnode1
+    rpc_send nvmf_subsystem_add_listener --trtype rdma \
+	     --traddr 2.2.103.1 \
+	     --adrfam ipv4 \
+	     --trsvcid 4420 \
+	     nqn.2016-06.io.spdk:cnode1
+
+    local i=0
+    for pci in $DISKS; do
+	rpc_send bdev_nvme_attach_controller --name Nvme$i \
+		 --trtype pcie \
+		 --traddr 0000:$pci:00.0
+	rpc_send nvmf_subsystem_add_ns nqn.2016-06.io.spdk:cnode1 Nvme${i}n1
+	((i+=1))
+    done
+    rpc_stop
+    sleep 1
+}
+
+function test_1()
+{
+    start_tgt 0xFFFF
+    config_null_1
+    FIO_JOB=fio-1ns basic_test
+    stop_tgt
+}
+
+function test_2()
+{
+    start_tgt 0xFFFF
+    config_null_16
+    FIO_JOB=fio-16ns basic_test
+    stop_tgt
+}
+
+function test_3()
+{
+    start_tgt 0xFFFF
+    config_nvme
+    FIO_JOB=fio-16ns basic_test
+    stop_tgt
+}
+
+function test_4()
+{
+    start_tgt 0xFFFF
+    NUM_SHARED_BUFFERS=96 BUF_CACHE_SIZE=6 config_null_16
+    FIO_JOB=fio-16ns basic_test
+    stop_tgt
+}
+
+function test_5()
+{
+    start_tgt 0xFFFF
+    NUM_SHARED_BUFFERS=96 BUF_CACHE_SIZE=6 config_nvme
+    FIO_JOB=fio-16ns basic_test
+    stop_tgt
+}
+
+function test_8()
+{
+    for cache_size in 6 3 1 0; do
+	echo "Buffer cache size $cache_size"
+	start_tgt 0xFFFF
+	NUM_SHARED_BUFFERS=96 BUF_CACHE_SIZE=$cache_size config_nvme
+	FIO_JOB=fio-16ns basic_test
+	stop_tgt
+	sleep 3
+    done
+}
+
+function test_9()
+{
+    for num_buffers in 128 96 64 48 32; do
+	local cache_size=$((num_buffers/16))
+	echo "Num shared buffers $num_buffers. Buffer cache size $cache_size"
+	start_tgt 0xFFFF
+	NUM_SHARED_BUFFERS=$num_buffers BUF_CACHE_SIZE=$cache_size config_nvme
+	FIO_JOB=fio-16ns basic_test
+	stop_tgt
+	sleep 3
+    done
+}
+
+
+if [ -z "$1" ]; then
+    declare -F | grep ' -f test' | cut -d " " -f 3
+    exit 0
+fi
+
+for test in $@; do
+    echo "$test"
+    $test
+done
